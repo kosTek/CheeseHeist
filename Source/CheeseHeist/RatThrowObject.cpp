45063f7f2d3980a6c2575a48759f03bd9c67d2f0
kosTek <kostekofficial@gmail.com>
2023-10-13 21:00:35 +0100

    Rat Throw Skeletal Mesh

--- Source/CheeseHeist/RatThrowObject.cpp
+++ Source/CheeseHeist/RatThrowObject.cpp
@@ -2,19 +2,17 @@
 
 
 #include "RatThrowObject.h"
-#include "Components/StaticMeshComponent.h"
+#include "Components/SkeletalMeshComponent.h"
 
 // Sets default values
 ARatThrowObject::ARatThrowObject() {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MeshComponent"));
 	SetRootComponent(Mesh);
 
 	Mesh->SetSimulatePhysics(true);
-
-
 }
 
 // Called when the game starts or when spawned
