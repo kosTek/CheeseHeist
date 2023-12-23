1dd53f1b933bb946934d9802ce4fa2fe8025968d
kosTek <kostekofficial@gmail.com>
2023-12-23 23:10:45 +0000

    Structure & file changes

--- Source/CheeseHeist/HeistGamemode.cpp
+++ Source/CheeseHeist/HeistGamemode.cpp
@@ -1,14 +1,11 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
-#include "CheeseHeistGameMode.h"
-#include "CheeseHeistCharacter.h"
-#include "UObject/ConstructorHelpers.h"
 
-ACheeseHeistGameMode::ACheeseHeistGameMode()
-	: Super()
-{
+#include "HeistGamemode.h"
+
+AHeistGamemode::AHeistGamemode() : Super() {
+
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
-
 }
--- Source/CheeseHeist/PlayerHUD.h
+++ Source/CheeseHeist/PlayerHUD.h
@@ -6,12 +6,17 @@
 #include "Blueprint/UserWidget.h"
 #include "PlayerHUD.generated.h"
 
-/**
- * 
- */
+
+class UTextBlock;
+
 UCLASS()
 class CHEESEHEIST_API UPlayerHUD : public UUserWidget
 {
 	GENERATED_BODY()
 	
+	/** Displays current objective text */
+	UPROPERTY(EditAnywhere, meta = (BindWidget))
+	UTextBlock* ObjectiveText;
+	
+
 };
