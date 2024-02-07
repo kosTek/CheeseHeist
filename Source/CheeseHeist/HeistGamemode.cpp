ab3ac8914f9e65822ca7c9773ebd7501195355d5
kosTek <kostekofficial@gmail.com>
2024-02-07 18:55:36 +0000

    Extraction start

--- Source/CheeseHeist/HeistGamemode.cpp
+++ Source/CheeseHeist/HeistGamemode.cpp
@@ -49,11 +49,19 @@ void AHeistGamemode::BeginPlay() {
 
 void AHeistGamemode::OnObjectiveFinished() {
 
+	HUD->SetObjectiveText(ObjectiveHandler->GetCurrentObjectiveText());
+
 	UE_LOG(LogTemp, Warning, TEXT("[Gamemode] Delegate Called"));
 
 }
 
 void AHeistGamemode::OnAllObjectivesFinished() {
+
+	// Check if all cheese has been collected
+		// Display secondary objective if not
+		// If all collected instruct to extract
+
+	HUD->SetObjectiveText("All objectives finished!"); // Debug
 
 	UE_LOG(LogTemp, Warning, TEXT("[Gamemode] Delegate Called"));
 
